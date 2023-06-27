c94a8373b49d82dd67b6cee03ad12cce013713a3
jianjundu <v@jianwi.cn>
2023-06-27 11:29:39 +0800

    openai proxy

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require('body-parser');
+
+const port = 9901;
+
+const app = express();
+app.use(bodyParser.json()); // 解析JSON格式的请求体
+const OPENAI_API_HOST = "api.openai.com";
+
+app.all("*", async (req, res) => {
+    const { headers, params,path } = req;
+    let newUrl = `https://${OPENAI_API_HOST}/${path}`
+
+    delete headers.host
+    delete headers.origin
+    delete headers.referer
+
+    try {
+        // 发送请求至 api.openai.com
+        const response = await axios.post(newUrl, req.body, {
+            headers: {
+                ...headers,
+            }
+        });
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send("Error");
+    }
+});
+
+app.listen(port, () => {
+    console.log("Server listening on http://localhost:"+port);
+});
